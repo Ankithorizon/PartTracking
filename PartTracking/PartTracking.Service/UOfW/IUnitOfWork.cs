2e3a0bbb94582e7615334258515cab824a6f60f2
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2021-08-17 16:19:49 -0500

    adding-service-context

---
+++ PartTracking/PartTracking.Service/UOfW/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using PartTracking.Service.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartTracking.Service.UOfW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IOrderMasterRepository OrderMasters { get; }
+        IPartMasterRepository PartMasters { get; }
+        IPartDetailRepository PartDetails { get; }
+        IReceivingRepository ReceiveParts { get; }
+        ICustomerWorkOrderRepository CustomerWorkOrders { get; }
+        int Complete();
+    }
+}
